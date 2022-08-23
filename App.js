64cc6dba72bb51c2c20c16a8123e9e2b68978c53
leonardo-fabricio <leonnardofabbricio@gmail.com>
2022-08-23 14:25:29 -0300

    page-detail

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/pages/home/index';
+import Detail from './src/pages/detail';
 
 export default function App() {
   return (
-   <Home/>
+   <Detail />
   );
 }
 
---
+++ src/pages/detail/index.js
@@ -0,0 +1,69 @@
+import { Text, SafeAreaView, Image } from "react-native";
+import styled from "styled-components";
+import file from "../../images/image-product.png";
+import {
+  ViewAux,
+  ViewContent,
+  ViewHeader,
+  ViewPrice,
+  ViewMain,
+  PriceProduct,
+  FromPrice,
+  DescriptionProduct,
+  TitleDescription,
+  TitleProduct,
+  ViewDescription,
+  ButtonAddCar,
+  QuantityCar,
+  NumberDelivery,
+  ViewQuantityDelivery,
+} from "./style";
+
+function Detail() {
+  return (
+    <SafeAreaView>
+      <ViewMain>
+        <ViewHeader>
+          <Image source={file} />
+        </ViewHeader>
+        <ViewContent>
+          <TitleProduct>Carlsberg Premium Beer</TitleProduct>
+
+          <ViewAux></ViewAux>
+          <ViewAux>
+            <ViewPrice>
+              <FromPrice>Por</FromPrice>
+              <PriceProduct>R$ 3,50</PriceProduct>
+            </ViewPrice>
+            <ViewQuantityDelivery>
+              <Text>menos</Text>
+              <Text>1</Text>
+              <Text>mais</Text>
+            </ViewQuantityDelivery>
+          </ViewAux>
+          <ViewAux>
+            <ViewDescription>
+              <TitleDescription>Descrição</TitleDescription>
+              <DescriptionProduct>
+                Carlsberg Smooth, a premium mild beer crafted with the finest
+                European Barley for a rich and smooth taste. Brewed specially
+                for the Indian palate with a refreshing and elegant taste,
+                Carlsberg Smooth offers a unique product and a new experience to
+                our consumers
+              </DescriptionProduct>
+            </ViewDescription>
+          </ViewAux>
+
+          <ButtonAddCar>
+            <Text>Adicionar ao Carrinho</Text>
+            <QuantityCar>
+              <NumberDelivery>3</NumberDelivery>
+            </QuantityCar>
+          </ButtonAddCar>
+        </ViewContent>
+      </ViewMain>
+    </SafeAreaView>
+  );
+}
+
+export default Detail;
