4a626ecfe1b71e7ebd55691e2c45e3fa28b8ef90
luis-luciqm <redbullfc18@gmail.com>
2022-09-02 14:11:58 -0300

    adicionando consumo da api de endereço

--- src/pages/address/index.js
+++ src/pages/address/index.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { 
     ViewContainer,
     TextCity,
@@ -21,7 +21,55 @@ import {
     ViewImage,
 } from './style';
 
+import axios from 'axios';
+
 export default function Address() {
+
+    const onPress1 = async function(){
+
+        let access = ''
+
+        await axios.post('http://207.246.118.151/api/user/login/', {
+            email: 'admin@gmail.com',
+            password: 'ifrn12345'
+          })
+          .then(function (response) {
+            console.log(response.data.tokens.access);
+            access = response.data.tokens.access
+          })
+          .catch(function (error) {
+            console.log(error);
+        });
+
+        await axios.post('http://207.246.118.151/api/user/cadastrar_novo_endereco/', 
+        {
+            cep: "5999000",
+            estado: "Brasil",
+            cidade: "Rafael Fernandes",
+            rua: "São Benedito",
+            numero: 12,
+            bairro: "Zona Rural",
+            tipo: "Casa",
+            complemento: "Pechinchou"
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${access}`,
+            },
+        }
+        )
+          .then(function (response) {
+            console.log('\n\n\n\n\n\n\n\n\n\n' + access)
+            console.log(response.data);
+            alert(`Endereço salvo com sucesso! \n\n ${JSON.stringify(response.data)}`)
+          })
+          .catch(function (error) {
+            console.log('\n\n\n\n\n\n\n\n\n\n' + access)
+            console.log(error);
+          });
+
+    };
+
     return (
         <ViewContainer>
             <ViewCity>
@@ -56,10 +104,21 @@ export default function Address() {
                 </ButtonsSalvarComo>
             </ViewSalvarComo>
 
-            <ButtonSave>
+            <TouchableOpacity style={styles.button_save} onPress={onPress1}>
                 <TextButtonSave>Salvar Endereço</TextButtonSave>
-            </ButtonSave>
+            </TouchableOpacity>
 
         </ViewContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    button_save: {
+        width: 340,
+        height: 40,
+        top: 80,
+        
+        backgroundColor: '#F9B233',
+        borderRadius: 10,
+    }
+});
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./pages/home";
 import Detail from "./pages/detail";
+import SearchAddress from "./pages/searchAddress";
+import Address from "./pages/address";
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="address" component={Address} />
+      <Stack.Screen name="searchaddress" component={SearchAddress} />
       <Stack.Screen name="home" component={Home} />
       <Stack.Screen name="detail" component={Detail} />
     </Stack.Navigator>
